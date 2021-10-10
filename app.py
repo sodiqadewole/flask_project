23e95ba24f625d83eedb6852e8516273c11755eb
sodiqadewole <adewolesodiq@gmail.com>
2021-10-10 16:36:38 -0700

    another commit

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return train() #'Hello World!';
+def home():  # put application's code here
+    return "Welcome to my website!";
 
-def train():
-    return 'train me ' + test()
+@app.route('/<name1>')
+def greetings(name):
+    return "Welcome to the "+name+"!"
 
 @app.route('/test')
 def test():
     return "new_test!"
 
 if __name__ == '__main__':
-    app.run(debug = True, host = "0.0.0.0", port = 5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
